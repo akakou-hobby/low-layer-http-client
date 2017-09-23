1c8ba85126013bff9f452b4eb33c69b1b8a334b8
akakou <s16003@tokyo.kosen-ac.jp>
2017-09-23 20:21:22 +0900

    [fix & add] fix miss and add get response and close connection

--- low_layer_http_client.py
+++ low_layer_http_client.py
@@ -3,12 +3,12 @@
 low_layler_http_client.py
     This program send HTTP request with TCP and get response.
 '''
+import socket
 
 
 class TCP():
     '''Send http request with TCP and get response.'''
     request = {
-        'url': '',
         'host': '',
         'port': 80,
         'header': '',
@@ -16,23 +16,48 @@ class TCP():
     }
 
     response = {
+        'raw': '',
+        'all': '',
         'header': '',
         'body': '',
-        'encode': 'utf-8'
+        'decode': 'utf-8'
     }
 
-    def __init__(self, url, host, port):
+    def __init__(self, host, port):
         '''Get arguments and set information for response packet.'''
-        self.request['url'] = url
         self.request['host'] = host
         self.request['port'] = port
 
-    def connect():
+    def connect(self):
         '''Connect to target server.'''
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((host, port))
+        self.sock.connect((self.request['host'], self.request['port']))
 
     def send(self, body):
         '''Send HTTP request'''
+        self.request['body'] = body
         encode = self.request['encode']
-        self.sock.send(encode)
+        self.sock.send(body.encode(encode))
+
+    def get_response(self):
+        '''Get HTTP Response'''
+        data = []
+
+        chunk = 'null'
+        while chunk!=b'':
+            chunk = self.sock.recv(4096)
+            data.append(chunk)
+
+        self.response['raw'] = b''.join(data)
+        decode = self.response['decode']
+        self.response['all'] = self.response['raw'].decode(decode)
+        divide_response = self.response['all'].split('\r\n\r\n')
+
+        self.response['header'] = ''.join(divide_response[0])
+        self.response['body'] = ''.join(divide_response[1:])
+
+        return self.response
+
+    def close(self):
+        '''Close HTTP connection'''
+        self.sock.close()
